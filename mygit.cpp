7cc6773dbc5e8fa22d7b40be6ccc2cca9c756495
vishal2398 <54908864+vishal2398@users.noreply.github.com>
2019-11-09 02:21:54 +0530

    updated init and status merged

--- mygit.cpp
+++ mygit.cpp
@@ -1,98 +1,112 @@
-#include<unistd.h>
-#include<bits/stdc++.h>
-#include<sys/stat.h>
-#include<sys/types.h>
-#include<fstream> 
+#include <unistd.h>
+#include <bits/stdc++.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fstream>
+#include "status.hpp"
 using namespace std;
-int init(){
+int init()
+{
     bool gitdircreate = false;
     bool gitchdirmygit = false;
-    bool  gitv0create = false;
+    bool gitv0create = false;
     bool gitchdirv0 = false;
     bool gitcreateindexv0 = false;
 
-
-    if(mkdir(".mygit",0777) == -1)
-        {
-        cerr<<"Error1: "<<strerror(errno) <<endl;
+    if (mkdir(".mygit", 0777) == -1)
+    {
+        cerr << "Error1: " << strerror(errno) << endl;
         return 0;
-        }
-    else{
+    }
+    else
+    {
         gitdircreate = true;
         // cout<<".Mygit directory created"<<endl;
-        }
-
-    if(chdir(".mygit")==-1){
-    cerr<<"Error2: "<<strerror(errno)<<endl;
-    return 0;
-    }
-    else{
-    gitchdirmygit = true;
-    // cout<<"Moved to .mygit directory!!"<<endl;
     }
 
-
-   ofstream fout;
-   fout.open("version_no.txt",ios::out);
-   if(fout){
-       string num ="0";
-       fout<<num<<endl;
-   }
-   fout.close();
-
-if(mkdir("0",0777) == -1)
-        {
-        cerr<<"Error3: "<<strerror(errno) <<endl;
+    if (chdir(".mygit") == -1)
+    {
+        cerr << "Error2: " << strerror(errno) << endl;
         return 0;
-        }
-    else{
+    }
+    else
+    {
+        gitchdirmygit = true;
+        // cout<<"Moved to .mygit directory!!"<<endl;
+    }
+
+    ofstream fout;
+    fout.open("version_no.txt", ios::out);
+    if (fout)
+    {
+        string num = "0";
+        fout << num << endl;
+    }
+    fout.close();
+
+    if (mkdir("0", 0777) == -1)
+    {
+        cerr << "Error3: " << strerror(errno) << endl;
+        return 0;
+    }
+    else
+    {
         gitv0create = true;
         // cout<<"version0 directory created"<<endl;
-        }
-
-    if(chdir("0")==-1){
-    cerr<<"Error4: "<<strerror(errno)<<endl;
-    return 0;
     }
-    else{
-    gitchdirv0 = true;
-    // cout<<"Moved to .version0 directory!!"<<endl;
+
+    if (chdir("0") == -1)
+    {
+        cerr << "Error4: " << strerror(errno) << endl;
+        return 0;
     }
- 
- ofstream fout2;
- fout2.open("index.txt");
-//  cout<<"Created index.txt in v0 file"<<endl;
- gitcreateindexv0=true;
-fout.close();
+    else
+    {
+        gitchdirv0 = true;
+        // cout<<"Moved to .version0 directory!!"<<endl;
+    }
 
+    ofstream fout2;
+    fout2.open("index.txt");
+    //  cout<<"Created index.txt in v0 file"<<endl;
+    gitcreateindexv0 = true;
+    fout.close();
 
-if(gitcreateindexv0 && gitdircreate && gitv0create && gitchdirmygit && gitchdirv0)return 1;
-else return 0;
-};
-
-void status(){
-
+    if (gitcreateindexv0 && gitdircreate && gitv0create && gitchdirmygit && gitchdirv0)
+        return 1;
+    else
+        return 0;
 };
 
 void add(){
 
 };
 
+int main(int argc, char *argv[])
+{
+    string cmd;
 
-int main( int argc, char *argv[]){
-     string cmd;
-
-     while(true){
-         cmd = argv[1];
-         if(cmd == "init") {
-             if(init()){
-                 cout<<"Mygit initialised!!\n";
-                 exit(EXIT_SUCCESS);
- 
-             }else{
-                 cout<<"Sorry! Error in initialisation!!"<<endl;
-                 exit(EXIT_SUCCESS);
-             }
-     }
-}
+    while (true)
+    {
+        cmd = argv[1];
+        //cout << argv[1] << endl;
+        if (cmd == "init")
+        {
+            if (init())
+            {
+                cout << "Mygit initialised!!\n";
+                exit(EXIT_SUCCESS);
+            }
+            else
+            {
+                cout << "Sorry! Error in initialisation!!" << endl;
+                exit(EXIT_SUCCESS);
+            }
+        }
+        else if (cmd == "status")
+        {
+            status();
+            exit(EXIT_SUCCESS);
+        }
+    }
 }
